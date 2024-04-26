286f5727f3878bca73108634e639d9f1e6dda220
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-04-26 11:56:11 +0200

    int converter error fix

--- revolt/ext/commands/errors.py
+++ revolt/ext/commands/errors.py
@@ -13,7 +13,7 @@ __all__ = (
     "ConverterError",
     "InvalidLiteralArgument",
     "BadBoolArgument",
-    "IntConverterError"
+    "IntConverterError",
     "CategoryConverterError",
     "ChannelConverterError",
     "TextChannelConverterError",
